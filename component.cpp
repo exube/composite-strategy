42408bb26568d5908d078722ac9fd376d69bb5e8
Daniel Ma <daniel.r.ma@gmail.com>
2017-10-24 10:12:49 -0700

    added definitions for evaluate()

---
+++ component.cpp
@@ -0,0 +1,18 @@
+#include "component.h"
+
+double Op::evaluate() { return this->data; }
+
+double Multiply::evaluate() { 
+    return this->left->evaluate() * this->right->evaluate(); 
+}
+
+
+double Divide::evaluate() { 
+    return this->left->evaluate() / this->right->evaluate(); 
+}
+double Add::evaluate() { 
+    return this->left->evaluate() + this->right->evaluate(); 
+}
+double Subtract::evaluate() { 
+    return this->left->evaluate() - this->right->evaluate(); 
+}
