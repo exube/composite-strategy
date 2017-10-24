2ac3049ee7d253b4de8400c72c52c4154a31a1c7
Daniel Ma <daniel.r.ma@gmail.com>
2017-10-24 11:10:26 -0700

    implemented sorts

---
+++ selectionsort.cpp
@@ -0,0 +1,28 @@
+#include "selectionsort.h"
+#include "container.h"
+void SelectionSort::sort(Container* container) {
+
+    int size = container->size();
+    
+    for (int i = 0; i < size; i++) {
+        int largest = 0;
+
+        // Get the largest remaining element
+        for (int j = i; j < size; j++) {
+            if (container->at(largest)->evaluate() < container->at(j)->evaluate()) {
+                largest = j;
+            }
+        }
+      
+        // If the largest is larger than current element, swap
+
+        if (container->at(largest)->evaluate() > container->at(i)->evaluate()) {
+            container->swap(i, largest);
+        }
+
+    }
+}
+            
+
+    
+  
