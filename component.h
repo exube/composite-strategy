a1387fe9dee1d9179977065f1c81b2ad6722254f
Daniel Ma <daniel.r.ma@gmail.com>
2017-10-24 10:06:34 -0700

    Added component class declarations

--- component.h
+++ component.h
@@ -10,4 +10,52 @@ class Base {
         virtual double evaluate() = 0;
 };
 
+class Op : public Base {
+    private:
+        double data;
+    public:
+        Op(double d) : data(d) {}
+        
+        double evaluate();
+            
+}
+
+class Multiply : public Base {
+    private:
+        Base* left;
+        Base* right;
+    public:
+        Multiply( Base* l, Base* r ) : left(l), right(r) {}
+
+        double evaluate();
+}
+
+
+class Divide : public Base {
+    private:
+        Base* left;
+        Base* right;
+    public:
+        Divide( Base* l, Base* r ) : left(l), right(r) {}
+
+        double evaluate();
+}
+class Add : public Base {
+    private:
+        Base* left;
+        Base* right;
+    public:
+        Add( Base* l, Base* r ) : left(l), right(r) {}
+
+        double evaluate();
+}
+class Subtract : public Base {
+    private:
+        Base* left;
+        Base* right;
+    public:
+        Subtract( Base* l, Base* r ) : left(l), right(r) {}
+
+        double evaluate();
+}
 #endif // __COMPONENT_H__
